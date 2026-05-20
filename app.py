bf09bc439b5fcbf2f4dbce4ac919b48943180212
Gokulkrishnar10 <rajendrangokulkrishna24@gmail.com>
2026-05-20 14:18:24 +0530

    add health endpoint

--- app.py
+++ app.py
@@ -598,6 +598,11 @@ def download():
     )
 
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
